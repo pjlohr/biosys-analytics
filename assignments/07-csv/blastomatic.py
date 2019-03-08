5347862b3061f23dca7c8aadb60ec1ba2ff70a49
Patrick Lohr <patricklohr@Patricks-MacBook-Pro.local>
2019-03-07 22:44:39 -0700

    Getting there

--- assignments/07-csv/blastomatic.py
+++ assignments/07-csv/blastomatic.py
@@ -61,32 +61,30 @@ def main():
     out = args.outfile
     ann = args.annotations
     FILE = args.FILE
+    results = {}
+    if not os.path.isfile(out) and out != '':  # check if out directory exists, and make one if false
+        open(out, 'w')
 
-    if not os.path.exists(out) and out != '':  # check if out directory exists, and make one if false
-        os.makedirs(out)
-
-    if not os.path.exists(FILE):
+    if not os.path.isfile(FILE):
         die('\"{}\" is not a file'.format(FILE))
 
-    if not os.path.exists(ann) and ann != '':
+    if not os.path.isfile(ann) and ann != '':
         die('\"{}\" is not a file'.format(ann))
 
-    with open(FILE) as csvfile:
+    with open(FILE) as csvfile, open(ann) as annfile:
         dict_args = {'delimiter': '\t'}
-        names = ['qaccver','saccver','pident','length','mismatch','gapopen','qstart','qend','sstart','sendevalue','bitscore']
+        blastnames = ['qaccver','saccver','pident','length','mismatch','gapopen','qstart','qend','sstart','sendevalue','bitscore']
+        blastreader = csv.DictReader(csvfile, fieldnames = blastnames, **dict_args)
+        annreader = csv.DictReader(annfile, **dict_args)
 
-        reader = csv.DictReader(csvfile, fieldnames = names, **dict_args)
+        for i, brow in enumerate(blastreader, start=1):
+            for j, arow in enumerate(annreader, start=1):
+                blastvals = dict(brow)
+                annvals = dict(arow)
 
-        for i, row in enumerate(reader, start=1):
-            vals = dict(row)
-            flds = vals.keys()
-            fmt = '{} : {}'
-            print('// ****** Record {} ****** //'.format(i))
-            n = 0
-            for key, val in vals.items():
-                n += 1
-                show = fmt.format(key, val)
-                print(show)
+                for bkey, bval in blastvals.items():
+                    for akey, aval in annvals.items():
+                        print(akey)
 
 # --------------------------------------------------
 if __name__ == '__main__':

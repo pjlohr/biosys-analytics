26077091eea9e528959117b36e88667c900aac27
Patrick Lohr <pjlohr@email.arizona.edu>
2019-04-03 17:10:28 -0700

    passing pass

---
+++ assignments/12-regex-passwords/pass.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+Author : patricklohr
+Date   : 2019-04-02
+Purpose: Rock the Casbah
+"""
+
+import os
+import sys
+import re
+
+
+# --------------------------------------------------
+def main():
+    args = sys.argv[1:]
+
+    if len(args) != 2:
+        print('Usage: {} PASSWORD ALT'.format(os.path.basename(sys.argv[0])))
+        sys.exit(1)
+
+    password, alt = args
+
+    ucfirst = password[0].upper() + password[1:]
+
+    ok = (password == alt) or (password.upper() == alt) or (
+        ucfirst == alt) or re.match('.?' + password + '.?', alt)
+
+    print('ok' if ok else 'nah')
+
+
+# --------------------------------------------------
+main()

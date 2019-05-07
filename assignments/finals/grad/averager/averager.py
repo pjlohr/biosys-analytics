562cfa7443c3de8d99b97f48642a85f0a4a18663
Patrick Lohr <pjlohr@email.arizona.edu>
2019-05-07 08:14:00 -0700

    Progress

--- assignments/finals/grad/averager/averager.py
+++ assignments/finals/grad/averager/averager.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 """
-Author : kyclark
-Date   : 2019-04-22
-Purpose: Average all the numbers in a document
+Author : patricklohr
+Date   : 2019-05-07
+Purpose: Rock the Casbah
 """
 
 import argparse
-import os
-import re
 import sys
+import re
+import os
 
 
 # --------------------------------------------------
@@ -41,23 +41,10 @@ def die(msg='Something bad happened'):
 def main():
     """Make a jazz noise here"""
     args = get_args()
+    FILES = args.FILE
 
-    num_re = re.compile('-?\d+(?:\.\d+)?')
-    for file in args.file:
-        if not os.path.isfile(file):
-            warn('"{}" is not a file'.format(file))
-            continue
-
-        nums = num_re.findall(open(file).read())
-        avg = 0
-
-        if nums:
-            nums = list(map(float, nums))
-            avg = sum(nums)/len(nums)
-
-        print('{:10.02f}: {}'.format(avg, os.path.basename(file)))
-
-
+    for file in FILES:
+        if
 # --------------------------------------------------
 if __name__ == '__main__':
     main()
